4bba5eb090d1d6529fa3114c00cea34e4c964e28
Francois Volschenk <francoisv934@gmail.com>
2022-11-09 09:33:28 +0200

    Set up structure to display first page

--- ht_analysis/website/views.py
+++ ht_analysis/website/views.py
@@ -1,3 +1,12 @@
+import code
+from multiprocessing import context
 from django.shortcuts import render
+import random
 
 # Create your views here.
+def index(request):
+    Context = {"numbers": []}
+    for i in range(10):
+        Context["numbers"].append(random.randint(0, 100))
+
+    return render(request, "website/index.html", Context)
